7883be1bb69f1819a4778a67e820e31cb7fc11cb
raynantamayo-netizen <raynan.tamayo@extenteam.com>
2025-11-04 00:45:21 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 const SUPABASE_URL = "https://tvhunlkpuekapgkkgtkf.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2aHVubGtwdWVrYXBna2tndGtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NjU5NTcsImV4cCI6MjA3NjU0MTk1N30.cYBUGbWPosqKl7ecg2MwDOxsRSh3JGbo_7cCJ1ErRAQ";
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
@@ -79,3 +79,4 @@ async function loadAssessments() {
 }
 
 loadAssessments();
+
